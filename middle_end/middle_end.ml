67e379d38f77ca6a6d2880613fb60387ecf529a0
octachron <octa@polychoron.fr>
2017-06-28 20:18:30 +0200

    Middle end changes

--- middle_end/middle_end.ml
+++ middle_end/middle_end.ml
@@ -179,15 +179,15 @@ let middle_end ppf ~prefixname ~backend
                   (inline_and_simplify.ml line 710). *)
                Location.prerr_warning (Debuginfo.to_location apply.dbg)
                  (Warnings.Inlining_impossible
-                    "[@inlined] attribute was not used on this function \
+                    (I18n.s "[@inlined] attribute was not used on this function \
                      application (the optimizer did not know what function \
-                     was being applied)")
+                     was being applied)"))
              | Unroll _ ->
                Location.prerr_warning (Debuginfo.to_location apply.dbg)
                  (Warnings.Inlining_impossible
-                    "[@unroll] attribute was not used on this function \
+                    (I18n.s"[@unroll] attribute was not used on this function \
                      application (the optimizer did not know what function \
-                     was being applied)"));
+                     was being applied)")));
            if !Clflags.dump_flambda
            then
              Format.fprintf ppf "End of middle end:@ %a@."
