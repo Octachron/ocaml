3f2f881725acdd47df80a888d59ac3f7adeeff7b
Florian Angeletti <florian.angeletti@inria.fr>
2019-12-11 18:44:59 +0100

    typemod: basic application error message

--- typing/includemod.mli
+++ typing/includemod.mli
@@ -90,3 +90,10 @@ exception Error of explanation
 
 val report_error: formatter -> explanation -> unit
 val expand_module_alias: Env.t -> Path.t -> Types.module_type
+
+type functor_app_patch
+val functor_app_diff:
+  Env.t -> f:Typedtree.module_expr -> args:Typedtree.module_expr list ->
+  functor_app_patch option
+val pp_functor_app_patch:
+  [< `Left | `Right] -> Format.formatter -> functor_app_patch -> unit
