8c77a61cb27541e2c7512b1a7e5d2f36d754b941
Florian Angeletti <florian.angeletti@inria.fr>
2022-02-14 17:13:16 +0100

    WIP: test generator

--- testsuite/tests/lib-dynlink-domains/generator.ml
+++ testsuite/tests/lib-dynlink-domains/generator.ml
@@ -8,13 +8,31 @@ and forest = node array
 
 let () = Random.init 10
 
+module Pos: sig
+  type t
+  val exn: int -> t
+  val opt: int -> t option
+  val rand: t -> int
+  val (+): t -> t -> t
+  val (-): t -> int -> t option
+  val int: t -> int
+end = struct
+  type t = int
+  let opt n = if n > 0 then Some n else None
+  let exn n = if n > 0 then n else failwith "Non-positive argument"
+  let rand n = if n <= 0 then failwith (Printf.sprintf "positive int error: %d" n) else Random.int n
+  let ( + ) = ( + )
+  let ( - ) x y = opt (x - y)
+  let int x = x
+end
+
 let rec drop n l = if n = 0 then l else
     match l with
     | [] -> []
     | a :: q -> drop (n-1) q
 
 let rand_parent d path =
-  drop (1 + Random.int d) path
+  drop (1 + Pos.rand d) path
 
 
 let id ppf path =
@@ -26,7 +44,7 @@ let name ppf path =
   Format.fprintf ppf "Unit%a" id path
 
 let rand_parents ~ncalls ~depth path =
-  Array.init (1 + Random.int ncalls) (fun _ -> rand_parent depth path)
+  Array.init (1 + Pos.rand ncalls) (fun _ -> rand_parent depth path)
 
 let leaf ~ncalls ~depth ~domains path  =
   let sync = if domains >= 1 then Par else Seq in
@@ -35,29 +53,34 @@ let leaf ~ncalls ~depth ~domains path  =
 
 
 let split bins n =
-  if n = 0 then Array.init bins (fun _ -> 0)
-  else
-    let seps = Array.init (1+bins) (fun _ -> Random.int (n+1)) in
+  match Pos.opt n with
+  | None -> Array.init bins (fun _ -> 0)
+  | Some p ->
+    let seps = Array.init (1+bins) (fun _ -> Pos.(rand (p+ exn 1))) in
     let () = Array.sort Stdlib.compare seps in
     seps.(0) <- 0;
     seps.(bins) <- n;
     Array.init bins (fun i -> seps.(i+1) - seps.(i))
 
-let spawn_or_not ~current_depth ~domains ~depth = match current_depth with
-  | 0 -> domains, Seq
+let spawn_or_not ~current_depth ~domains ~depth = match Pos.int current_depth with
   | 1 -> domains - 1, Par
   | _ ->
       if domains = 0 then 0, Seq
-      else if domains >= depth || Random.int depth = 0 then
+      else if domains >= Pos.int depth || Pos.rand depth = 0 then
         domains - 1, Par
       else domains, Seq
 
 
 let rec rand path ~ncalls ~current_depth ~domains ~width ~depth =
-  if depth <= 0 || width <= 0 then
+  match depth, width <= 0 with
+  | None, _ | _, true ->
     leaf ~depth:current_depth ~domains ~ncalls path
-  else
-    let bins = 1 + Random.int (min width 4) in
+  | Some depth, false ->
+      let bins =
+        match Pos.opt (min width 4) with
+        | None -> 1
+        | Some p -> 1 + Pos.rand p
+      in
     branch path
       ~ncalls
       ~current_depth
@@ -74,9 +97,9 @@ and branch path ~ncalls ~current_depth ~domains ~width ~depth ~bins =
         rand (n::path)
           ~ncalls
           ~domains:(number_of_domains.(n))
-          ~current_depth:(current_depth+1)
+          ~current_depth:Pos.(current_depth+ exn 1)
           ~width:(number_of_grandchilds.(n))
-          ~depth:(depth-1)
+          ~depth:Pos.(depth-1)
       )
   in
   let parents = rand_parents ~ncalls ~depth path in
@@ -90,9 +113,9 @@ let start  ~ncalls ~domains ~width ~depth ~childs =
         rand [n]
           ~ncalls
           ~domains:2
-          ~current_depth:1
+          ~current_depth:(Pos.exn 1)
           ~width:(number_of_grandchilds.(n))
-          ~depth:(depth-1)
+          ~depth:Pos.(depth-1)
       )
   in
   let parents = [||] in
@@ -102,12 +125,12 @@ let start  ~ncalls ~domains ~width ~depth ~childs =
 
 let seq ppf target =
   Format.fprintf ppf
-    {|[@<h>let () = Dynlink.load @@ Dynlink.adapt_filename "%a.cmo"@]@.|}
+    {|@[<h>let () = Dynlink.loadfile @@@@ Dynlink.adapt_filename "%a.cmo"@]@.|}
     name target
 
 let par_create ppf target =
   Format.fprintf ppf
-    {|[@<h>let d%a = Domain.spawn (fun () -> Dynlink.load @@ Dynlink.adapt_filename "%a.cmo")@]@.|}
+    {|@[<h>let d%a = Domain.spawn (fun () -> Dynlink.loadfile @@@@ Dynlink.adapt_filename "%a.cmo")@]@.|}
     id target name target
 
 let add ppf parent = match parent with
@@ -116,7 +139,7 @@ let add ppf parent = match parent with
 
 let register ppf parent current =
   Format.fprintf ppf
-    {|[@<h>let () = %a "%a->%a"|}
+    {|@[<h>let () = %a "%a->%a"|}
     add parent
     id current
     id parent
@@ -124,14 +147,16 @@ let register ppf parent current =
 let new_add ppf node =
   let path =
     let n = Array.length node.parents in
-    if  n = 0 then [] else node.parents.(Random.int n)
+    match Pos.opt n with
+    | None -> []
+    | Some p -> node.parents.(Pos.rand p)
   in
   Format.fprintf ppf
     "let add x = %a x@." add path
 
 let par_join ppf target =
     Format.fprintf ppf
-    {|[@<h>let () = Domain.join d%a@]@.|}
+    {|@[<h>let () = Domain.join d%a@]@.|}
     id target
 
 type action = Parent of path | Child of node
@@ -139,7 +164,7 @@ type action = Parent of path | Child of node
 
 let shuffle a =
   let a = Array.copy a in
-  for i = Array.length a - 1 to 0 do
+  for i = Array.length a - 1 downto 1 do
     let pos = Random.int i in
     let tmp = a.(i) in
     a.(i) <- a.(pos);
@@ -163,10 +188,14 @@ let write_action self ppf = function
       | Seq -> seq ppf n.path
       | Par -> par_create ppf n.path
 
+let join_action ppf ch = match ch.sync with
+  | Seq -> ()
+  | Par -> par_join ppf ch.path
+
 let write_node ppf node =
   let schedule = schedule node in
   Array.iter (write_action node ppf) schedule;
-  Array.iter (par_join ppf) (shuffle node.parents);
+  Array.iter (join_action ppf) (shuffle node.children);
   new_add ppf node;
   Format.pp_print_flush ppf ()
 
@@ -179,7 +208,7 @@ let to_file name f x =
 
 
 let gen_node node =
-  to_file (Format.asprintf "%a.ml" name node.path)
+  to_file (Format.asprintf "@[<h>%a.ml@]" name node.path)
   write_node node
 
 
@@ -196,7 +225,7 @@ and iter_forest f nodes = Array.iter (iter f) nodes
 let gen_files node = iter gen_node node
 
 let files ppf node =
-  iter (fun node -> Format.fprintf ppf " %a.ml" name node.path) node
+  iter (fun node -> Format.fprintf ppf " @[<h>%a.ml@]" name node.path) node
 
 
 let registred ppf node =
@@ -221,7 +250,7 @@ let main_header ppf node =
 
 include dynlink
 libraries = ""
-readonly_files = "store.ml main.ml%a"
+readonly_files = "@[<h>store.ml main.ml%a@]"
 
 * shared-libraries
 ** setup-ocamlc.byte-build-env
@@ -242,7 +271,7 @@ module = "store.ml"@.|}
      program = \"./run.byte.exe\"@ \
      libraries= \"dynlink\"@ \
      all_modules = \"store.cmo main.cmo\"@ \
-     module = \"\" @\
+     module = \"\" @ \
      %a run@ \
      %a check-program-output@ \
      @ \
@@ -318,7 +347,7 @@ let main_file ppf node =
   check ppf node
 
 let () =
-  let domains = 16 in
-  let tree = start ~ncalls:5 ~depth:10 ~domains ~width:10 ~childs:(domains/2) in
+  let domains = 4 in
+  let tree = start ~ncalls:(Pos.exn 5) ~depth:(Pos.exn 1) ~domains ~width:10 ~childs:(domains/2) in
   to_file "main.ml" main_file tree;
   gen_files tree
