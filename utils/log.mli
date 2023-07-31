18f7e48125ee537ff0614ab1757d60da092dd7cf
Florian Angeletti <florian.angeletti@inria.fr>
2023-07-31 16:53:41 +0200

    Check-typo

--- utils/log.mli
+++ utils/log.mli
@@ -1,3 +1,18 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Florian Angeletti, projet Cambium, Inria Paris             *)
+(*                                                                        *)
+(*   Copyright 2023 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
 (** The log module provides an unified interface for logging in
     a structured log
 *)
