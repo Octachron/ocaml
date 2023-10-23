ab0f8212355fe3421cb5abd7a1ce94ad5c9dbfdb
Florian Angeletti <florian.angeletti@inria.fr>
2023-10-23 16:04:13 +0200

    Fix check-typo

--- tools/ci/actions/check-parsetree-modified.sh
+++ tools/ci/actions/check-parsetree-modified.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+#**************************************************************************
+#*                                                                        *
+#*                                 OCaml                                  *
+#*                                                                        *
+#*                 David Allsopp, OCaml Labs, Cambridge.                  *
+#*                                                                        *
+#*   Copyright 2021 David Allsopp Ltd.                                    *
+#*                                                                        *
+#*   All rights reserved.  This file is distributed under the terms of    *
+#*   the GNU Lesser General Public License version 2.1, with the          *
+#*   special exception on linking described in the file LICENSE.          *
+#*                                                                        *
+#**************************************************************************
+
 
 set -e
 
