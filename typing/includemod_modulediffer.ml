4de6d4bb856e604f824cb3d0a4245284f7a39d35
MDLC01 <57839069+MDLC01@users.noreply.github.com>
2024-07-09 16:02:59 +0200

    Fix indentation

--- typing/includemod_modulediffer.ml
+++ typing/includemod_modulediffer.ml
@@ -125,9 +125,9 @@ module Stable_marriage_diff = struct
 
   type 'a preferences = {
     mutable previous_layers : 'a;
-      (** Invariant: if this is a list, it is not empty. *)
+        (** Invariant: if this is a list, it is not empty. *)
     mutable current_layer : int list;
-      (** Invariant: this list is not empty. *)
+        (** Invariant: this list is not empty. *)
     mutable current_layer_distance : distance;
     mutable next_layers : (int list * distance) Seq.t;
   }
@@ -143,7 +143,7 @@ module Stable_marriage_diff = struct
     | Active of man_situation
     | Engaged_man of man_situation
     | Inactive
-      (** Old bachelor. *)
+        (** Old bachelor. *)
 
   type woman_state =
     | Maiden
